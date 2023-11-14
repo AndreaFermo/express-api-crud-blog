33dc0a069a6486e8a430679daca16f3d2058006a
Andrea Fermo <andreafermo.sd@gmail.com>
2023-11-14 15:35:32 +0100

    base per l'esercizio di oggi

--- routers/posts.js
+++ routers/posts.js
@@ -2,4 +2,15 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts");
 
+router.get("/", postsController.index);
+
+router.get("/create", postsController.create);
+
+router.get("/:slug", postsController.show);
+
+router.get("/:slug/download", postsController.downloadImage);
+
+router.get("/:slug/image", postsController.showImage);
+
+
 module.exports = router;
